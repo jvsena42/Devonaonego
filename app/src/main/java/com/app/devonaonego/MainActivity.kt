1453215197327510948842258c2bd9f41b6f442b
jvsena42 <jvsena16@gmail.com>
2020-06-13 16:46:01 -0300

    Config Firebase

--- app/src/main/java/com/app/devonaonego/MainActivity.kt
+++ app/src/main/java/com/app/devonaonego/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.app.devonaonego
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun telaCadastro(view:View){
+        val intent = Intent(this,CadastroActivity::class.java)
+        startActivity(intent)
     }
 }
